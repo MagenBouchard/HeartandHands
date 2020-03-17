f0b3c30b0accf237aebd7e2bff2835f79378f942
Magen Bouchard <Magen.Bouchard@gmail.com>
2020-03-17 15:50:55 -0400

    Updates

---
+++ precache-manifest.8c6b37b5ee218b1afdf68d30226b26e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd71d8c3560d137cd72daa6e7e556276",
+    "url": "/HeartandHands/index.html"
+  },
+  {
+    "revision": "2cd300429cd026eeccfc",
+    "url": "/HeartandHands/static/css/main.3c5a74f7.chunk.css"
+  },
+  {
+    "revision": "71d6270ffe5f1ac92c0f",
+    "url": "/HeartandHands/static/js/2.f36665b1.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/HeartandHands/static/js/2.f36665b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cd300429cd026eeccfc",
+    "url": "/HeartandHands/static/js/main.4c35d3c7.chunk.js"
+  },
+  {
+    "revision": "ba13f12e8f1e7b1d654c",
+    "url": "/HeartandHands/static/js/runtime-main.eaee1391.js"
+  },
+  {
+    "revision": "9f6186585a812a10ea3f0dde090b1588",
+    "url": "/HeartandHands/static/media/background-tile.9f618658.png"
+  }
+]);
